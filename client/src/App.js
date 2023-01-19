1ebaec2a31cf545a0484c26bf9c911bf1d18dbcc
Anuragsrivastava997 <anuragsrivastava997@gmail.com>
2023-01-19 23:35:46 +0530

    navbar-completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,29 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
+import NavBar from "scenes/navbar";
+
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userid" element={<ProfilePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/navbar" element={<NavBar />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userid" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
